cce30ed9e76d9624ecdd74e74f43166f5f0908dc
Mitra Partogi <mitrapartogi@gmail.com>
2024-11-28 09:28:23 +0700

    nambahin semuanya

---
+++ src/app/magang/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Magang() {
+  const magang = "PGR"
+  return (
+    <div className="italic">
+      {magang}
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  const nama = "Mitra"
+  return (
+    <div className = "text-red-500">
+      
+      {nama}
+    </div>
+  );
+}
